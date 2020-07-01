63800e350cf83fa58380106f2cd9d547b9c5401f
Esteban Primost <esteban.primost@gmail.com>
2020-07-01 17:31:39 -0300

    Husky. Lint fixes

--- packages/cli/src/commands/start.js
+++ packages/cli/src/commands/start.js
@@ -1,10 +1,8 @@
-const { Command, flags } = require('@oclif/command')
+const { Command, flags } = require('@oclif/command');
 const seeder = require('@geut/seeder');
 
 class StartCommand extends Command {
-  async run() {
-    const { flags } = this.parse(StartCommand)
-
+  async run () {
     const keys = require('../../keys.json');
 
     this.log(JSON.stringify(keys, null, 2));
@@ -16,10 +14,10 @@ class StartCommand extends Command {
 StartCommand.description = `Describe the command here
 ...
 Extra documentation goes here
-`
+`;
 
 StartCommand.flags = {
-  name: flags.string({ char: 'n', description: 'name to print' }),
-}
+  name: flags.string({ char: 'n', description: 'name to print' })
+};
 
-module.exports = StartCommand
+module.exports = StartCommand;
--- packages/cli/tests/index.js
+++ packages/cli/tests/index.js
@@ -1,3 +1,3 @@
 test('demo test', () => {
-  expect('nice').toBe('nice')
-})
+  expect('nice').toBe('nice');
+});
