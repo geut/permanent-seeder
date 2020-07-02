bc47b3870970269246be54a0d51fffc854637cd7
Esteban Primost <esteban.primost@gmail.com>
2020-07-02 08:24:28 -0300

    Standard

--- packages/cli/src/commands/start.js
+++ packages/cli/src/commands/start.js
@@ -1,20 +1,20 @@
-const { Command } = require('@oclif/command');
-const seeder = require('@geut/seeder');
+const { Command } = require('@oclif/command')
+const seeder = require('@geut/seeder')
 
 class StartCommand extends Command {
   async run () {
-    const keys = require('../../keys.json');
+    const keys = require('../../keys.json')
 
-    this.log(JSON.stringify(keys, null, 2));
+    this.log(JSON.stringify(keys, null, 2))
 
-    seeder();
+    seeder()
   }
 }
 
-StartCommand.description = 'Start permanent seeder daemon';
+StartCommand.description = 'Start permanent seeder daemon'
 
 // StartCommand.flags = {
 //   name: flags.string({ char: 'n', description: 'name to print' })
 // };
 
-module.exports = StartCommand;
+module.exports = StartCommand
--- packages/seeder/index.js
+++ packages/seeder/index.js
@@ -1,5 +1,5 @@
 // your code starts here
 
 module.exports = function seeder () {
-  console.log('seeder---');
-};
+  console.log('seeder---')
+}
