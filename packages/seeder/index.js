5f91948999102efa16e863209b64959db3f534d7
Diego Paez <diego@geutstudio.com>
2020-07-03 19:27:24 -0300

    Update: initial work on seeder, very basic download

--- packages/seeder/index.js
+++ packages/seeder/index.js
@@ -1,5 +1,82 @@
-// your code starts here
+const { join } = require('path')
+const { homedir } = require('os')
+const Hyperdrive = require('@geut/hyperdrive-promise')
+const Corestore = require('corestore')
+const CSN = require('corestore-swarm-networking')
+const raf = require('random-access-file')
 
-module.exports = function seeder () {
-  console.log('seeder---')
+const DEFAULT_OPTS = {
+  announce: true,
+  lookup: true,
+  hyperdriveOpts: {
+    sparse: false,
+    latest: true
+  },
+  storageLocation: join(homedir(), 'permanent-seeder'),
+  corestoreOpts: {
+    sparse: false,
+    eagerUpdate: true
+  },
+  swarmOpts: {
+    announceLocalAddress: true
+  },
+  dhtOpts: {}
 }
+
+const getCoreStore = (storageLocation, name) => {
+  const location = join(storageLocation, name)
+  return file => raf(join(location, file))
+}
+
+class Seeder {
+  constructor (opts = {}) {
+    this.opts = { ...DEFAULT_OPTS, ...opts }
+    this.drives = new Map()
+  }
+
+  async init () {
+    this.store = new Corestore(
+      getCoreStore(this.opts.storageLocation, '.hyper'),
+      this.opts.corestoreOpts
+    )
+    await this.store.ready()
+
+    this.networker = new CSN(this.store, { ...this.opts.swarmOpts })
+  }
+
+  async seed (keys = []) {
+    for (const key of keys) {
+      const drive = Hyperdrive(this.store, key, this.hyperdriveOpts)
+      await drive.ready()
+      const { discoveryKey } = drive
+      this.drives.set(discoveryKey.toString('hex'), drive)
+      // join em all
+      await this.networker.join(discoveryKey, { announce: this.opts.announce, lookup: this.opts.lookup })
+      const handle = drive.download('/')
+      await new Promise(resolve => {
+        handle.on('finish', async () => {
+          const stats = await drive.stats('/')
+          console.log({ stats })
+          return resolve()
+        })
+      })
+    }
+  }
+
+  async unseed (dkey) {
+    if (dkey) {
+      return this.networker.leave(dkey)
+    }
+
+    // note(dk): I think this can be done in parallel with promise.all
+    for (const drive of this.drives.values()) {
+      await this.networker.leave(drive.discoveryKey)
+    }
+  }
+
+  async destroy () {
+    await this.networker.close()
+  }
+}
+
+module.exports = Seeder
