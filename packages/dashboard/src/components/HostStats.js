f35e89beeb49a967136329c1fcab1581aefd7b60
Diego Paez <diego@geutstudio.com>
2020-08-13 18:31:37 -0300

    Update: show disks data with tooltip

--- packages/dashboard/src/components/HostStats.js
+++ packages/dashboard/src/components/HostStats.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import useFetch from 'use-http'
 
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, withStyles } from '@material-ui/core/styles'
+import Avatar from '@material-ui/core/Avatar'
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import Button from '@material-ui/core/Button'
+import ClickAwayListener from '@material-ui/core/ClickAwayListener'
 import Typography from '@material-ui/core/Typography'
+import Tooltip from '@material-ui/core/Tooltip'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+import ListItemAvatar from '@material-ui/core/ListItemAvatar'
+import DataUsageIcon from '@material-ui/icons/DataUsage'
 
 import CircularProgress from './CircularProgress'
 
@@ -32,15 +41,73 @@ function Uptime ({ uptime }) {
   )
 }
 
+const HtmlTooltip = withStyles((theme) => ({
+  tooltip: {
+    backgroundColor: '#f5f5f9',
+    color: 'rgba(0, 0, 0, 0.87)',
+    maxWidth: 220,
+    fontSize: theme.typography.pxToRem(12),
+    border: '1px solid #dadde9'
+  }
+}))(Tooltip)
+
+const DiskData = ({ disks = [] }) => {
+  const [open, setOpen] = useState(false)
+  const diskItem = (datum, i) => (
+    <ListItem key={i}>
+      <ListItemAvatar>
+        <Avatar>
+          <DataUsageIcon />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary={datum._filesystem} secondary={datum._capacity} />
+    </ListItem>
+  )
+  const handleTooltipClose = () => {
+    setOpen(false)
+  }
+
+  const handleTooltipOpen = () => {
+    setOpen(true)
+  }
+
+  return (
+    <ClickAwayListener onClickAway={handleTooltipClose}>
+      <div>
+        <HtmlTooltip
+          PopperProps={{
+            disablePortal: true
+          }}
+          onClose={handleTooltipClose}
+          open={open}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title={
+            <>
+              <List>
+                {disks.map((d, idx) => diskItem(d, idx))}
+              </List>
+            </>
+          }
+        >
+          <Button onClick={handleTooltipOpen}>FS</Button>
+        </HtmlTooltip>
+      </div>
+    </ClickAwayListener>
+  )
+}
+
 function HostStats () {
   const classes = useStyles()
 
   const { get, response } = useFetch(API_URL)
-  const [stats, setStats] = useState({ cpu: 0, mem: 0, uptime: 0, loadavg: [0, 0, 0] })
+  const [stats, setStats] = useState({ cpu: 0, mem: 0, uptime: 0, loadavg: [0, 0, 0], disk: [] })
 
   useEffect(() => {
     async function fetchInitalData () {
       const hostStats = await get('/stats/host')
+      console.log({ hostStats })
       if (response.ok) setStats(hostStats)
     }
 
@@ -56,6 +123,7 @@ function HostStats () {
       <BottomNavigationAction label='Memory' icon={<CircularProgress value={Math.round(stats.mem * 100)} />} />
       <BottomNavigationAction label='Uptime' icon={<Uptime uptime={stats.uptime} />} />
       <BottomNavigationAction label='Load' icon={<CircularProgress value={stats.loadavg[1]} />} />
+      <BottomNavigationAction label='Disk Usage' icon={<DiskData disks={stats.disk} />} />
     </BottomNavigation>
   )
 }
