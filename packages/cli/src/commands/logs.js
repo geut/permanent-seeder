34ec6e0584ef0d6237b1ff6c3e5e401e11a886e5
Esteban Primost <esteban.primost@gmail.com>
2020-08-13 15:45:42 -0300

    Log command

---
+++ packages/cli/src/commands/logs.js
@@ -0,0 +1,38 @@
+const { join } = require('path')
+const { promises: { readFile } } = require('fs')
+
+const { flags } = require('@oclif/command')
+
+const Tail = require('tail').Tail
+
+const BaseCommand = require('../base-command')
+
+class TailCommand extends BaseCommand {
+  async run () {
+    const { flags: { error, live, all } } = this.parse(TailCommand)
+
+    const file = join(this.config.home, `.pm2/logs/seeder-daemon-${error ? 'error' : 'out'}.log`)
+
+    if (!live) {
+      const data = await readFile(file, 'utf8')
+      console.log(data)
+      process.exit(0)
+    }
+
+    const tail = new Tail(file, { fromBeginning: all, flushAtEOF: true })
+
+    tail.on('line', function (data) {
+      console.log(data)
+    })
+  }
+}
+
+TailCommand.description = 'Show logs'
+
+TailCommand.flags = {
+  error: flags.boolean({ default: false, description: 'Show error logs' }),
+  live: flags.boolean({ default: false, description: 'Keep logs live' }),
+  all: flags.boolean({ default: false, description: 'Show logs from beginning' })
+}
+
+module.exports = TailCommand
