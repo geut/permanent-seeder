f1a26d39a230a66f565c027cef4394fc16fa792d
Diego Paez <diego@geutstudio.com>
2020-08-13 16:01:07 -0300

    Update: use permanent drawer mode

--- packages/dashboard/src/components/AppBar.js
+++ packages/dashboard/src/components/AppBar.js
@@ -1,21 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import clsx from 'clsx'
 
-import { makeStyles } from '@material-ui/core/styles'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { useAppBarTitle } from '../hooks/layout'
 
 import MuiAppBar from '@material-ui/core/AppBar'
+import Drawer from '@material-ui/core/Drawer'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import Badge from '@material-ui/core/Badge'
 import MenuIcon from '@material-ui/icons/Menu'
-import NotificationsIcon from '@material-ui/icons/Notifications'
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
 
 import { DRAWER_WITH } from '../constants'
 
-import { useLeftSidebar, useAppBarTitle } from '../hooks/layout'
-
 const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex'
+  },
   toolbar: {
     paddingRight: 24 // keep right padding when drawer closed
   },
@@ -42,40 +45,102 @@ const useStyles = makeStyles((theme) => ({
   },
   title: {
     flexGrow: 1
+  },
+  hide: {
+    display: 'none'
+  },
+  drawer: {
+    width: DRAWER_WITH,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: DRAWER_WITH
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end'
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -DRAWER_WITH
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
   }
 }))
 
 function AppBar ({ drawerWith }) {
   const classes = useStyles()
 
-  const [open, setOpen] = useLeftSidebar()
-  const [appBarTitle] = useAppBarTitle()
+  const theme = useTheme()
+
+  const [open, setOpen] = useState(false)
+
+  const [appBarTitle, setAppBarTitle] = useAppBarTitle()
+
+  useEffect(() => {
+    setAppBarTitle('Dashboard')
+  }, [setAppBarTitle])
 
   const handleDrawerOpen = () => {
     setOpen(true)
   }
+
+  const handleDrawerClose = () => {
+    setOpen(false)
+  }
   return (
-    <MuiAppBar position='absolute' className={clsx(classes.appBar, open && classes.appBarShift)}>
-      <Toolbar className={classes.toolbar}>
-        <IconButton
-          edge='start'
-          color='inherit'
-          aria-label='open drawer'
-          onClick={handleDrawerOpen}
-          className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography component='h1' variant='h6' color='inherit' noWrap className={classes.title}>
-          {appBarTitle}
-        </Typography>
-        <IconButton color='inherit'>
-          <Badge badgeContent={4} color='secondary'>
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-      </Toolbar>
-    </MuiAppBar>
+    <div className={classes.root}>
+      <MuiAppBar
+        position='fixed'
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open
+        })}
+      >
+        <Toolbar className={classes.toolbar}>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='open drawer'
+            onClick={handleDrawerOpen}
+            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography component='h1' variant='h6' color='inherit' noWrap className={classes.title}>
+            {appBarTitle}
+          </Typography>
+        </Toolbar>
+      </MuiAppBar>
+      <Drawer
+        className={classes.drawer}
+        variant='persistent'
+        anchor='left'
+        open={open}
+        classes={{
+          paper: classes.drawerPaper
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+      </Drawer>
+    </div>
   )
 }
 
