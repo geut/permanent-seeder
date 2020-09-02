5dd2361fdebc611d3c9899cb44bb3544c9329b93
Esteban <esteban.primost@gmail.com>
2020-09-02 17:04:04 -0300

    Status command (#72)
    * Status command
    
    * Every 5 seconds

--- packages/cli/src/pm2-async.js
+++ packages/cli/src/pm2-async.js
@@ -3,12 +3,13 @@ const { promisify } = require('util')
 const pm2 = require('pm2')
 
 const pm2Connect = promisify(pm2.connect.bind(pm2))
+const pm2Describe = promisify(pm2.describe.bind(pm2))
 const pm2Disconnect = promisify(pm2.disconnect.bind(pm2))
+const pm2List = promisify(pm2.list.bind(pm2))
+const pm2Restart = promisify(pm2.restart.bind(pm2))
+const pm2SendDataToProcessId = promisify(pm2.sendDataToProcessId.bind(pm2))
 const pm2Start = promisify(pm2.start.bind(pm2))
 const pm2Stop = promisify(pm2.stop.bind(pm2))
-const pm2Restart = promisify(pm2.restart.bind(pm2))
-const pm2List = promisify(pm2.list.bind(pm2))
-const pm2SendDataToProcessId = promisify(pm2.sendDataToProcessId.bind(pm2))
 
 async function sendMessage (processName, message, data) {
   await pm2Connect()
@@ -37,10 +38,11 @@ async function sendMessage (processName, message, data) {
 }
 
 module.exports.pm2Connect = pm2Connect
+module.exports.pm2Describe = pm2Describe
 module.exports.pm2Disconnect = pm2Disconnect
+module.exports.pm2List = pm2List
+module.exports.pm2Restart = pm2Restart
+module.exports.pm2SendDataToProcessId = pm2SendDataToProcessId
 module.exports.pm2Start = pm2Start
 module.exports.pm2Stop = pm2Stop
-module.exports.pm2Restart = pm2Restart
-module.exports.pm2List = pm2List
-module.exports.pm2SendDataToProcessId = pm2SendDataToProcessId
 module.exports.sendMessage = sendMessage
