8f991f86c2a808a8fc67afbda7e88f8887aaebec
Diego Paez <diego@geutstudio.com>
2020-09-09 16:25:53 -0300

    Update: dynamic theme

--- packages/dashboard/src/containers/App.js
+++ packages/dashboard/src/containers/App.js
@@ -8,7 +8,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 
 import { play, exit } from '../timeline'
 
-import { AppStateProvider } from '../context/app-state'
+import { useDarkMode } from '../hooks/layout'
 
 import Layout from '../components/Layout'
 import NoMatch from '../components/NoMatch'
@@ -29,33 +29,10 @@ const ethSansTTF = {
   `
 }
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamilyGEUT: 'ethon, Arial'
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        '@font-face': [ethSansTTF]
-      }
-    }
-  },
-  palette: {
-    primary: {
-      main: '#7800D2',
-      light: '#ad47ff',
-      dark: '#3e009f'
-    },
-    secondary: {
-      main: '#5bd200',
-      light: '#93ff4d',
-      dark: '#0ca000'
-    }
-  }
-})
-
 function AppContainer () {
   const [intro, setIntro] = useState(true)
+  const [darkMode] = useDarkMode()
+
   useEffect(() => {
     async function delay () {
       await new Promise(resolve => setTimeout(resolve, 1700))
@@ -65,45 +42,69 @@ function AppContainer () {
     delay()
   }, [])
 
+  const theme = createMuiTheme({
+    typography: {
+      fontFamilyGEUT: 'ethon, Arial'
+    },
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          '@font-face': [ethSansTTF]
+        }
+      }
+    },
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#7800D2',
+        light: '#ae47ff',
+        dark: '#3f009f'
+      },
+      secondary: {
+        main: '#5bd200',
+        light: '#93ff4d',
+        dark: '#0ca000'
+      }
+    }
+  })
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <SocketIOProvider url='http://localhost:3001'>
-        <AppStateProvider>
-          <Router>
-            <Layout>
-              <Route render={({ location }) => {
-                const { pathname, key } = location
+        <Router>
+          <Layout>
+            <Route render={({ location }) => {
+              const { pathname, key } = location
 
-                return (
-                  <TransitionGroup component={null}>
-                    <Transition
-                      key={key}
-                      appear
-                      onEnter={(node, appears) => play(pathname, node, appears)}
-                      onExit={(node, appears) => exit(node, appears)}
-                      timeout={{ enter: 750, exit: 250 }}
-                    >
-                      <Switch>
-                        <Route exact path='/'>
-                          {intro ? <SplashScreen /> : <Redirect to='/dashboard' />}
-                        </Route>
-                        <Route path='/dashboard'>
-                          <Dashboard />
-                        </Route>
-                        <Route path='*'>
-                          <NoMatch />
-                        </Route>
-                      </Switch>
-                    </Transition>
-                  </TransitionGroup>
-                )
-              }}
-              />
+              return (
+                <TransitionGroup component={null}>
+                  <Transition
+                    key={key}
+                    appear
+                    onEnter={(node, appears) => play(pathname, node, appears)}
+                    onExit={(node, appears) => exit(node, appears)}
+                    timeout={{ enter: 750, exit: 250 }}
+                  >
+                    <Switch>
+                      <Route exact path='/'>
+                        {intro ? <SplashScreen /> : <Redirect to='/dashboard' />}
+                      </Route>
+                      <Route path='/dashboard'>
+                        <Dashboard />
+                      </Route>
+                      <Route path='*'>
+                        <NoMatch />
+                      </Route>
+                    </Switch>
+                  </Transition>
+                </TransitionGroup>
+              )
+            }}
+            />
 
-            </Layout>
-          </Router>
-        </AppStateProvider>
+          </Layout>
+        </Router>
       </SocketIOProvider>
     </ThemeProvider>
   )
