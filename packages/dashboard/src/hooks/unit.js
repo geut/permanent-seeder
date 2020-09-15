e5f0f5836e1c30952a2c889984cd19111dbd3007
Diego Paez <diego@geutstudio.com>
2020-09-15 17:22:21 -0300

    Update: unexpected response check

--- packages/dashboard/src/hooks/unit.js
+++ packages/dashboard/src/hooks/unit.js
@@ -1,6 +1,9 @@
 import { humanizedBytes, milisecondsToHms } from '../format'
 
 export function useHumanizedBytes (bytes = 0) {
+  if (typeof bytes !== 'number' || bytes !== Number(bytes)) {
+    bytes = 0
+  }
   const { humanized, unit, pretty } = humanizedBytes(bytes)
   return [humanized, unit, pretty]
 }
