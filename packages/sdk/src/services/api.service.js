b21153c3a03a13ba5e95d3be884ded0ea30109c7
Esteban Primost <esteban.primost@gmail.com>
2020-09-24 18:34:50 -0300

    https & port settings

--- packages/sdk/src/services/api.service.js
+++ packages/sdk/src/services/api.service.js
@@ -1,3 +1,4 @@
+const { readFileSync } = require('fs')
 const { join, dirname } = require('path')
 const { homedir } = require('os')
 
@@ -8,270 +9,287 @@ const { encode } = require('dat-encoding')
 const heapdump = require('heapdump')
 const dashboard = require.resolve('@geut/permanent-seeder-dashboard')
 
-module.exports = {
-  name: 'api',
+module.exports = function (broker) {
+  let { api: { https, port = 3001 } = {} } = broker.metadata.config
 
-  mixins: [ApiGatewayService],
+  try {
+    if (https) {
+      https = {
+        key: readFileSync(https.key),
+        cert: readFileSync(https.cert)
+      }
+    }
+  } catch (error) {
+    broker.logger.error(error)
+  }
 
-  dependencies: [
-    'metrics',
-    'seeder',
-    'keys'
-  ],
+  return {
+    name: 'api',
 
-  settings: {
-    port: 3001,
+    mixins: [ApiGatewayService],
 
-    // Dashboard site
-    assets: {
-      folder: dirname(dashboard)
-    },
-
-    whitelist: [
-      'api.*'
+    dependencies: [
+      'metrics',
+      'seeder',
+      'keys'
     ],
 
-    routes: [{
-      use: [
-        compression()
+    settings: {
+      port,
+
+      https,
+
+      // Dashboard site
+      assets: {
+        folder: dirname(dashboard)
+      },
+
+      whitelist: [
+        'api.*'
       ],
-      aliases: {
-        'GET api/drives/:key?': 'api.drives',
-        'GET api/drives/:key/size': 'api.drives.size',
-        'GET api/drives/:key/peers': 'api.drives.peers',
-        'GET api/drives/:key/stats': 'api.drives.stats',
-        'GET api/drives/:key/info': 'api.drives.info',
-        'GET api/stats/host': 'api.stats.host',
-        'GET api/stats/network': 'api.stats.network',
-        'GET api/raw/:key': 'api.raw',
-        'GET api/heapdump': 'api.heapdump',
-        'POST api/drives': 'api.drives.add'
-      }
-    }],
 
-    onError (req, res, err) {
-      res.setHeader('Content-Type', 'text/plain')
-      res.writeHead(501)
-      res.end(err.message)
-    },
-
-    cors: true
-  },
-
-  events: {
-    'seeder.drive.add' (ctx) {
-      this.io.emit('drive.add', ctx.params.key)
-    },
-
-    'seeder.drive.remove' (ctx) {
-      this.io.emit('drive.remove', ctx.params.key)
-    },
-
-    'seeder.drive.download' (ctx) {
-      this.io.emit(`drive.${ctx.params.key}.download`, ctx.params.key)
-    },
-
-    'seeder.drive.upload' (ctx) {
-      this.io.emit(`drive.${ctx.params.key}.upload`, ctx.params.key)
-    },
-
-    'seeder.drive.indexjson.update' (ctx) {
-      this.io.emit(`drive.${ctx.params.key}.indexjson.update`, ctx.params.key)
-    },
-
-    'seeder.drive.peer.add' (ctx) {
-      this.io.emit(`drive.${ctx.params.key}.peer.add`, ctx.params.key)
-    },
-
-    'seeder.drive.peer.remove' (ctx) {
-      this.io.emit(`drive.${ctx.params.key}.peer.remove`, ctx.params.key)
-    },
-
-    'stats.host' (ctx) {
-      this.io.emit('stats.host', ctx.params.stats)
-    },
-
-    'stats.network' (ctx) {
-      this.io.emit('stats.network', ctx.params.stats)
-    }
-
-  },
-
-  actions: {
-    drives: {
-      async handler (ctx) {
-        return this.drives(ctx.params.key)
-      }
-    },
-
-    'drives.size': {
-      async handler (ctx) {
-        return this.driveSize(ctx.params.key)
-      }
-    },
-
-    'drives.info': {
-      async handler (ctx) {
-        return this.driveInfo(ctx.params.key)
-      }
-    },
-
-    'drives.peers': {
-      async handler (ctx) {
-        return this.drivePeers(ctx.params.key)
-      }
-    },
-
-    'drives.stats': {
-      async handler (ctx) {
-        return this.driveStats(ctx.params.key)
-      }
-    },
-
-    'drives.add': {
-      async handler (ctx) {
-        await ctx.call('keys.add', {
-          key: encode(ctx.params.key)
-        })
-      }
-    },
-
-    'stats.network': {
-      async handler (ctx) {
-        return ctx.call('metrics.getNetworkStats')
-      }
-    },
-
-    'stats.host': {
-      async handler (ctx) {
-        return ctx.call('metrics.getHostStats')
-      }
-    },
-    raw: {
-      async handler (ctx) {
-        return this.raw(ctx.params.key)
-      }
-    },
-    'raw.event': {
-      async handler (ctx) {
-        return this.raw(ctx.params.key, ctx.params.event)
-      }
-    },
-    heapdump: {
-      async handler (ctx) {
-        return this.heapdump()
-      }
-    }
-  },
-
-  methods: {
-    driveSize: {
-      async handler (key) {
-        const size = await this.broker.call('seeder.driveSize', { key })
-        return size
-      }
-    },
-
-    driveInfo: {
-      async handler (key) {
-        const info = await this.broker.call('seeder.driveInfo', { key })
-        return info
-      }
-    },
-
-    drivePeers: {
-      async handler (key) {
-        const peers = await this.broker.call('seeder.drivePeers', { key })
-        return peers
-      }
-    },
-
-    driveStats: {
-      async handler (key) {
-        const stats = await this.broker.call('seeder.driveStats', { key })
-        return stats
-      }
-    },
-
-    drives: {
-      async handler (key) {
-        let keys = []
-
-        if (key) {
-          keys.push(await this.broker.call('keys.get', { key }))
-        } else {
-          keys = await this.broker.call('keys.getAll')
+      routes: [{
+        use: [
+          compression()
+        ],
+        aliases: {
+          'GET api/drives/:key?': 'api.drives',
+          'GET api/drives/:key/size': 'api.drives.size',
+          'GET api/drives/:key/peers': 'api.drives.peers',
+          'GET api/drives/:key/stats': 'api.drives.stats',
+          'GET api/drives/:key/info': 'api.drives.info',
+          'GET api/stats/host': 'api.stats.host',
+          'GET api/stats/network': 'api.stats.network',
+          'GET api/raw/:key': 'api.raw',
+          'GET api/heapdump': 'api.heapdump',
+          'POST api/drives': 'api.drives.add'
         }
+      }],
 
-        let drives = []
+      onError (req, res, err) {
+        res.setHeader('Content-Type', 'text/plain')
+        res.writeHead(501)
+        res.end(err.message)
+      },
 
-        drives = await Promise.all(keys.map(async ({ key: publicKey }) => {
-          return {
-            key: {
-              publicKey
-            },
-            stats: await this.driveStats(publicKey),
-            size: await this.driveSize(publicKey),
-            peers: await this.drivePeers(publicKey)
-          }
-        }))
-
-        return key ? drives[0] : drives
-      }
+      cors: true
     },
-    raw: {
-      async handler (key, timestamp) {
-        // get all keys from timestamp (optional)
-        const stats = await this.broker.call('metrics.get', { key, timestamp })
-        return stats
-      }
-    },
-    heapdump: {
-      async handler () {
-        return new Promise((resolve, reject) => {
-          const dest = join(homedir(), `heapDump-${Date.now()}.heapsnapshot`)
-          heapdump.writeSnapshot(dest, (err) => {
-            if (err) {
-              this.logger.error(err)
-              return reject(err)
-            }
 
-            return resolve({ dest })
+    events: {
+      'seeder.drive.add' (ctx) {
+        this.io.emit('drive.add', ctx.params.key)
+      },
+
+      'seeder.drive.remove' (ctx) {
+        this.io.emit('drive.remove', ctx.params.key)
+      },
+
+      'seeder.drive.download' (ctx) {
+        this.io.emit(`drive.${ctx.params.key}.download`, ctx.params.key)
+      },
+
+      'seeder.drive.upload' (ctx) {
+        this.io.emit(`drive.${ctx.params.key}.upload`, ctx.params.key)
+      },
+
+      'seeder.drive.indexjson.update' (ctx) {
+        this.io.emit(`drive.${ctx.params.key}.indexjson.update`, ctx.params.key)
+      },
+
+      'seeder.drive.peer.add' (ctx) {
+        this.io.emit(`drive.${ctx.params.key}.peer.add`, ctx.params.key)
+      },
+
+      'seeder.drive.peer.remove' (ctx) {
+        this.io.emit(`drive.${ctx.params.key}.peer.remove`, ctx.params.key)
+      },
+
+      'stats.host' (ctx) {
+        this.io.emit('stats.host', ctx.params.stats)
+      },
+
+      'stats.network' (ctx) {
+        this.io.emit('stats.network', ctx.params.stats)
+      }
+
+    },
+
+    actions: {
+      drives: {
+        async handler (ctx) {
+          return this.drives(ctx.params.key)
+        }
+      },
+
+      'drives.size': {
+        async handler (ctx) {
+          return this.driveSize(ctx.params.key)
+        }
+      },
+
+      'drives.info': {
+        async handler (ctx) {
+          return this.driveInfo(ctx.params.key)
+        }
+      },
+
+      'drives.peers': {
+        async handler (ctx) {
+          return this.drivePeers(ctx.params.key)
+        }
+      },
+
+      'drives.stats': {
+        async handler (ctx) {
+          return this.driveStats(ctx.params.key)
+        }
+      },
+
+      'drives.add': {
+        async handler (ctx) {
+          await ctx.call('keys.add', {
+            key: encode(ctx.params.key)
           })
-        })
+        }
+      },
+
+      'stats.network': {
+        async handler (ctx) {
+          return ctx.call('metrics.getNetworkStats')
+        }
+      },
+
+      'stats.host': {
+        async handler (ctx) {
+          return ctx.call('metrics.getHostStats')
+        }
+      },
+      raw: {
+        async handler (ctx) {
+          return this.raw(ctx.params.key)
+        }
+      },
+      'raw.event': {
+        async handler (ctx) {
+          return this.raw(ctx.params.key, ctx.params.event)
+        }
+      },
+      heapdump: {
+        async handler (ctx) {
+          return this.heapdump()
+        }
       }
+    },
+
+    methods: {
+      driveSize: {
+        async handler (key) {
+          const size = await this.broker.call('seeder.driveSize', { key })
+          return size
+        }
+      },
+
+      driveInfo: {
+        async handler (key) {
+          const info = await this.broker.call('seeder.driveInfo', { key })
+          return info
+        }
+      },
+
+      drivePeers: {
+        async handler (key) {
+          const peers = await this.broker.call('seeder.drivePeers', { key })
+          return peers
+        }
+      },
+
+      driveStats: {
+        async handler (key) {
+          const stats = await this.broker.call('seeder.driveStats', { key })
+          return stats
+        }
+      },
+
+      drives: {
+        async handler (key) {
+          let keys = []
+
+          if (key) {
+            keys.push(await this.broker.call('keys.get', { key }))
+          } else {
+            keys = await this.broker.call('keys.getAll')
+          }
+
+          let drives = []
+
+          drives = await Promise.all(keys.map(async ({ key: publicKey }) => {
+            return {
+              key: {
+                publicKey
+              },
+              stats: await this.driveStats(publicKey),
+              size: await this.driveSize(publicKey),
+              peers: await this.drivePeers(publicKey)
+            }
+          }))
+
+          return key ? drives[0] : drives
+        }
+      },
+      raw: {
+        async handler (key, timestamp) {
+        // get all keys from timestamp (optional)
+          const stats = await this.broker.call('metrics.get', { key, timestamp })
+          return stats
+        }
+      },
+      heapdump: {
+        async handler () {
+          return new Promise((resolve, reject) => {
+            const dest = join(homedir(), `heapDump-${Date.now()}.heapsnapshot`)
+            heapdump.writeSnapshot(dest, (err) => {
+              if (err) {
+                this.logger.error(err)
+                return reject(err)
+              }
+
+              return resolve({ dest })
+            })
+          })
+        }
+      }
+    },
+
+    async started () {
+      this.io = IO.listen(this.server)
+
+      this.io.on('connection', client => {
+        this.logger.info('SOCKET: Client connected via websocket!')
+
+        client.on('disconnect', () => {
+          this.logger.info('SOCKET: Client disconnected')
+        })
+
+        client.on('drive.size', async (key, done) => {
+          const size = await this.driveSize(key)
+          done(size)
+        })
+
+        client.on('drive.peers', async (key, done) => {
+          const peers = await this.drivePeers(key)
+          done(peers)
+        })
+
+        client.on('drive.stats', async (key, done) => {
+          const stats = await this.driveStats(key)
+          done(stats)
+        })
+
+        client.on('drive.info', async (key, done) => {
+          const info = await this.driveInfo(key)
+          done(info)
+        })
+      })
     }
-  },
-
-  async started () {
-    this.io = IO.listen(this.server)
-
-    this.io.on('connection', client => {
-      this.logger.info('SOCKET: Client connected via websocket!')
-
-      client.on('disconnect', () => {
-        this.logger.info('SOCKET: Client disconnected')
-      })
-
-      client.on('drive.size', async (key, done) => {
-        const size = await this.driveSize(key)
-        done(size)
-      })
-
-      client.on('drive.peers', async (key, done) => {
-        const peers = await this.drivePeers(key)
-        done(peers)
-      })
-
-      client.on('drive.stats', async (key, done) => {
-        const stats = await this.driveStats(key)
-        done(stats)
-      })
-
-      client.on('drive.info', async (key, done) => {
-        const info = await this.driveInfo(key)
-        done(info)
-      })
-    })
   }
 }
