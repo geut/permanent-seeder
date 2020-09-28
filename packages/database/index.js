c9066d41de43bf76c14b8cc40999e42eb10fa1d6
Diego Paez <diego@geutstudio.com>
2020-09-28 19:45:57 -0300

    Update: expose new db

--- packages/database/index.js
+++ packages/database/index.js
@@ -1,7 +1,9 @@
 const Database = require('./src/database')
 const KeysDatabase = require('./src/keys-database')
 const MetricsDatabase = require('./src/metrics-database')
+const DrivesDatabase = require('./src/drives-database')
 
 module.exports.Database = Database
 module.exports.KeysDatabase = KeysDatabase
 module.exports.MetricsDatabase = MetricsDatabase
+module.exports.DrivesDatabase = DrivesDatabase
