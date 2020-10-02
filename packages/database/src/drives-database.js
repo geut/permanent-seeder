f338b3589f5ee040a3797c6d19833def6975b385
Esteban Primost <esteban.primost@gmail.com>
2020-10-02 13:00:21 -0300

    drive database tweaks

--- packages/database/src/drives-database.js
+++ packages/database/src/drives-database.js
@@ -1,20 +1,9 @@
 const assert = require('assert')
+
 const Database = require('./database')
-const sub = require('subleveldown')
 
 class DrivesDatabase extends Database {
-  constructor (params) {
-    super(params)
-    // create local indexes
-    this.drives = sub(this._db, 'drives', { valueEncoding: 'json' })
-  }
-
-  _buildKey (keyParts) {
-    return ['drives', ...keyParts].join('!')
-  }
-
   _cleanKeyData (data = {}) {
-    console.log({ data })
     assert(Buffer.isBuffer(data.key) || typeof data.key === 'string', 'key should be string or buffer')
     const key = typeof data.key === 'string' ? data.key : data.key.toString('hex')
 
@@ -26,18 +15,30 @@ class DrivesDatabase extends Database {
 
   async onPreSet (key, data) {
     const cleanedData = this._cleanKeyData(data)
-    await this.drives.put(key, cleanedData)
     return cleanedData
   }
 
   /**
    *
    * @param {object} data keyRecord to add
-   *
-   * @returns {boolean} true if existent and key was updated
    */
   async add (data) {
-    await this.set(data.key, data)
+    const initialData = {
+      stats: {},
+      size: {
+        blocks: 0,
+        bytes: 0,
+        downloadedBlocks: 0
+      },
+      peers: [],
+      info: {},
+      seedingStatus: 'WAITING',
+      updatedAt: Date.now(),
+      createdAt: Date.now(),
+      ...data
+    }
+
+    await this.set(data.key, initialData)
   }
 
   /**
@@ -45,14 +46,27 @@ class DrivesDatabase extends Database {
    * @param {object} data keyRecord to update
    *
    */
-  async update (key, prop, data) {
-    let val = await this.get(key)
-    if (!val) {
-      val = { key }
-    }
-    val[prop] = data
+  async update (key, data = {}) {
+    const drive = await this.get(key)
 
-    await this.set(key, val)
+    if (!drive) {
+      await this.add(key, data)
+      return
+    }
+
+    await this.set(key, {
+      ...drive,
+      ...data,
+      updatedAt: Date.now()
+    })
+  }
+
+  async remove (key) {
+    const drive = await this.get(key)
+
+    if (!drive) return
+
+    this.update(key, { deletedAt: Date.now() })
   }
 }
 
