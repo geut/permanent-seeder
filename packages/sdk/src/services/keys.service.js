b73326ccb7a1c2715fc94784fa5bec550081781e
Diego Paez <diego@geutstudio.com>
2020-10-10 17:04:35 -0300

    Update: clean cache on keys update

--- packages/sdk/src/services/keys.service.js
+++ packages/sdk/src/services/keys.service.js
@@ -86,6 +86,9 @@ module.exports = {
       const updated = updateResult.filter(({ updated }) => updated).map(({ keyRecord }) => keyRecord)
       const created = updateResult.filter(({ created }) => created).map(({ keyRecord }) => keyRecord)
 
+      if (updated.length || created.length) {
+        this.broker.cacher.clean()
+      }
       updated.length && this.broker.broadcast('keys.updated', { keys: updated })
       created.length && this.broker.broadcast('keys.created', { keys: created })
     }
