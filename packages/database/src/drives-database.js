dc0a402b8b7eb437f4b425ddd37a4c6e1a580bb0
Diego Paez <diego@geutstudio.com>
2021-07-29 10:15:31 -0300

    Update: new db methods

--- packages/database/src/drives-database.js
+++ packages/database/src/drives-database.js
@@ -47,13 +47,64 @@ class DrivesDatabase extends Database {
     return field ? data[field] : data
   }
 
-  async update (key, data = {}) {
-    await this._db.batch([
+  async getByVersion (key, version, field) {
+    const fields = field ? [field] : FIELDS
+    const data = {}
+
+    for (const field of fields) {
+      data[field] = await super.get(key, 'version', version, field)
+    }
+
+    if (!field && !data.key) return null
+
+    return field ? data[field] : data
+  }
+
+  async getVersions (key) {
+    const stream = super.createKeyStream([key, 'version'])
+
+    return new Promise(resolve => {
+      const versions = new Set()
+
+      stream
+        .on('data', (key) => {
+          var version = key.split('!')[2]
+          if (version) versions.add(version)
+        })
+        .on('end', () => resolve(Array.from(versions)))
+    })
+  }
+
+  async getKeys () {
+    const stream = super.createKeyStream()
+
+    return new Promise(resolve => {
+      const out = []
+      stream
+        .on('data', (key) => {
+          out.push(key)
+        })
+        .on('end', () => resolve(out))
+    })
+  }
+
+  async update (key, data = {}, version) {
+    const batch = [
       ...(Object.entries(data).map(([field, value]) => ({
         type: 'put', key: this._buildKey([key, field]), value
       }))),
       { type: 'put', key: this._buildKey([key, 'updatedAt']), value: Date.now() }
-    ])
+    ]
+
+    if (version) {
+      batch.push(
+        ...(Object.entries(data).map(([field, value]) => ({
+          type: 'put', key: this._buildKey([key, 'version', version, field]), value
+        })))
+      )
+    }
+
+    await this._db.batch(batch)
   }
 
   async remove (key) {
